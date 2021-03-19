7ac5a793c2928b82f980de18ab2a770bdaaffd91
Krae <kraewind@gmail.com>
2021-03-18 22:10:23 -0400

    commit tree be11a473d0786316c95c7fb39b7cb178b7ae2ec7

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+    def display_artist(s)
+        if s.artist != nil
+            link_to s.artist_name, artist_path(s.artist)
+        else
+            link_to "Add Artist", edit_song_path(s)
+        end
+    end
 end
